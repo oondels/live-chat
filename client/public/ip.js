530d8cdde844afde93364050fbd29502e0dcea5c
Hendrius Félix <hendriusfelix@gmail.com>
2024-09-29 22:39:07 -0300

    ip change

--- client/public/ip.js
+++ client/public/ip.js
@@ -1,2 +1,2 @@
-export const ip = "http://localhost:2399";
-// export const ip = "https://live-chat-b304260d434c.herokuapp.com";
+// export const ip = "http://localhost:2399";
+export const ip = "https://live-chat-b304260d434c.herokuapp.com";
--- ip.js
+++ ip.js
@@ -1,4 +1,4 @@
-const ip = "http://localhost:2399";
-// const ip = "https://live-chat-b304260d434c.herokuapp.com";
+// const ip = "http://localhost:2399";
+const ip = "https://live-chat-b304260d434c.herokuapp.com";
 
 module.exports = ip;
